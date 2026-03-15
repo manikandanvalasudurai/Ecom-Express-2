9cd61aee409c017259744c5d96824314394cd192
Manikandan <manikandanvalasudurai@gmail.com>
2026-03-15 17:17:26 +0530

    Models Created

--- src/main/java/com/example/productservice/models/Product.java
+++ src/main/java/com/example/productservice/models/Product.java
@@ -1,4 +1,50 @@
 package com.example.productservice.models;
 
-public class Product {
+
+public class Product extends BaseModel {
+    private String title;
+    private  Double price;
+    private String description;
+    private String image;
+    private Category category;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
