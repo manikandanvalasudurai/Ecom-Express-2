e88f5ba7b97e884d5283da00ab3b96e367d05544
Manikandan <manikandanvalasudurai@gmail.com>
2026-06-16 08:35:20 +0530

    Completed types of Queries

--- src/main/java/com/example/productservice/repositories/ProductRepository.java
+++ src/main/java/com/example/productservice/repositories/ProductRepository.java
@@ -2,6 +2,7 @@ package com.example.productservice.repositories;
 import com.example.productservice.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -26,4 +27,16 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findOnlyProductTitleById(Long productId);
     //update + insert = upsert
     Product save(Product product);
+
+    //HQL Query -> we can use models
+    @Query("select p from products p where p.id = :productId") //Since we overrided the product table name it HQL will access product table as products only
+    Product findProductByGivenIdHQL(@Param("productId") Long productId);
+
+
+    //SQL Query
+    @Query(
+            value = "select * from products where id = :productId",
+            nativeQuery = true // means i am writing aql query don't do intervention on this
+    )
+    Product findProductByGivenIdSQL(@Param("productId") Long productId);
 }
--- src/test/java/com/example/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/example/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.productservice;
 
+import com.example.productservice.models.Product;
+import com.example.productservice.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
+    private final ProductRepository productRepository;
+    @Autowired
+    public ProductServiceApplicationTests(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void testQuery() {
+        Product product = productRepository.findProductByGivenIdHQL(1L);
+        System.out.println("DEBUG");
+    }
+
 
 }
